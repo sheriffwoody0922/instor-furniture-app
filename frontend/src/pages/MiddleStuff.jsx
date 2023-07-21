2d1e06fed00af28e5f2a2ea02b4d06dc12a4f38d
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-07-21 14:58:49 +0200

    upload frontend

---
+++ frontend/src/pages/MiddleStuff.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+
+const MiddleStuff = () => {
+    return ( 
+        <>
+        <Header />
+        </>
+     );
+}
+ 
+export default MiddleStuff;
---
+++ frontend/src/pages/SmallStuff.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const SmallStuff = () => {
+    return ( 
+        <>
+            <Header />
+        </>
+     );
+}
+ 
+export default SmallStuff;
