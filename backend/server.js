800f04fd9480e16ad84def2f5013f2fbed8df97f
sheriffwoody0922 <codewarrior0922@gmail.com>
2023-07-24 16:15:23 +0200

    added deploy config for render.com

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,19 @@ import {
 } from "./controllers/furnitureController.js";
 import "./config/config.js";
 import "./models/index.js";
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FE_DIR = join(__dirname, "../frontend/dist/");
+app.use(express.static(FE_DIR));
 dotenv.config();
 
 const PORT = 3001;
+const FE_INDEX = join(__dirname, "../frontend/dist/index.html");
 
 // Routen
 app.get("/api/furniture", getAllFurniture);
@@ -20,6 +29,8 @@ app.get("/api/furniture/:id", getFurnitureById);
 app.post("/api/addFurniture", upload.single("image"), addFurniture);
 app.put("/api/updateFurniture/:id", upload.single("image"), updateFurniture);
 app.delete("/api/deleteFurniture/:id", deleteFurniture);
+
+app.get("*", (req, res) => res.sendFile(FE_INDEX));
 
 app.listen(PORT, () => {
   console.log("Server läuft auf: ", PORT);
